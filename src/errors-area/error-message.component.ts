0bbdf31ea44696e0b092f385984237d5c7a26dd3
Alvaro Contreras <alvaro.contreras@base22.com>
2016-11-02 15:16:51 -0600

    Add property stack to Message interface. Add section to display stack when message has that property.

--- src/errors-area/error-message.component.ts
+++ src/errors-area/error-message.component.ts
@@ -1,18 +1,21 @@
-import { Component, Input, Output, SimpleChange, EventEmitter, OnChanges } from "@angular/core";
+import { Component, Input, Output, ElementRef, SimpleChange, EventEmitter, OnChanges, AfterViewInit } from "@angular/core";
 
 import $ from "jquery";
 import "semantic-ui/semantic";
 
 import template from "./error-message.component.html!";
+import style from "./error-message.component.css!text";
 
 @Component( {
 	selector: "cp-error-message",
 	template: template,
-	styles: [ ":host{ display:block; }" ],
+	styles: [ style ],
 } )
 
-export class ErrorMessageComponent implements OnChanges {
+export class ErrorMessageComponent implements OnChanges, AfterViewInit {
 
+	private element:ElementRef;
+	private $element:JQuery;
 	@Input() title:string;
 	@Input() content:string;
 	@Input() statusCode:string;
@@ -21,14 +24,22 @@ export class ErrorMessageComponent implements OnChanges {
 	@Input() message:Message;
 	@Input() errors:any[];
 	@Input() closable:boolean = false;
+	@Input() stack:string;
 	@Output() onClose:EventEmitter<any> = new EventEmitter();
 
-	constructor() { }
+	constructor( element:ElementRef ) {
+		this.element = element;
+		this.$element = $( this.element.nativeElement );
+	}
 
 	ngOnChanges( changes:{[propName:string]:SimpleChange} ):void {
 		if( ! ! changes[ "message" ].currentValue && changes[ "message" ].currentValue !== changes[ "message" ].previousValue ) {
 			this.decomposeMessage();
 		}
+	}
+
+	ngAfterViewInit():void {
+		this.$element.find( ".ui.stack.accordion" ).accordion();
 	}
 
 	private decomposeMessage():void {
@@ -38,6 +49,7 @@ export class ErrorMessageComponent implements OnChanges {
 		this.statusMessage = this.message.statusMessage;
 		this.endpoint = this.message.endpoint;
 		this.errors = this.message.errors;
+		this.stack = this.message.stack;
 	}
 
 	close( event:Event, messageDiv:HTMLElement ):void {
@@ -49,12 +61,13 @@ export class ErrorMessageComponent implements OnChanges {
 }
 
 export interface Message {
-	title:string;
-	content:string;
-	statusCode:string;
-	statusMessage:string;
-	endpoint:string;
-	errors:any[];
+	title?:string;
+	content?:string;
+	statusCode?:string;
+	statusMessage?:string;
+	endpoint?:string;
+	errors?:any[];
+	stack?:string;
 }
 
 export default ErrorMessageComponent;
