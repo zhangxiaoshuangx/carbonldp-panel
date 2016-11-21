99c569913de73e45a5c5b70aef979eeab17c0db9
Alvaro Contreras <alvaro.contreras@base22.com>
2016-11-21 13:12:25 -0600

    Add Stylesheet/css file to AgentsComponent to display default text as black

--- src/my-apps/app-content/security/agents/agents.component.ts
+++ src/my-apps/app-content/security/agents/agents.component.ts
@@ -3,11 +3,12 @@ import { Component, Input } from "@angular/core";
 import * as App from "carbonldp/App";
 
 import template from "./agents.component.html!";
+import style from "./agents.component.css!text";
 
 @Component( {
 	selector: "cp-agents",
 	template: template,
-	styles: [ ":host{ display:block; }" ],
+	styles: [ style ],
 } )
 
 export class AgentsComponent {
