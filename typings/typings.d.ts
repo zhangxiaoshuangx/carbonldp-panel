1ea4a4e2501adba6dee3f0b01a881afe93af70e9
Alvaro Contreras <alvaro.contreras@base22.com>
2017-03-25 12:45:40 -0600

    Remove unnecessary declarations of modules

--- typings/typings.d.ts
+++ typings/typings.d.ts
@@ -3,12 +3,3 @@
 /// <reference path="./custom/jstree/index.d.ts" />
 /// <reference path="./custom/semantic-ui/index.d.ts" />
 
-declare module "*.html!" {
-	let value:string;
-	export default value;
-}
-
-declare module "*!text" {
-	let value:string;
-	export default value;
-}
